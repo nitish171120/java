9840265c89adc2e49f9f29ec02e2a1a3bca11827
nitish171120 <70973642+nitish171120@users.noreply.github.com>
2020-11-01 19:46:34 +0530

    Day 2
    Day 2 assignment

---
+++ Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	employee e = new employee();
+	e.Display();
+	employee e1 = new employee();
+	e1.name = "saurabh";
+	e1.age = 23;
+	e1.city = "New Delhi";
+	e1.Display();
+    }
+
+}
---
+++ employee.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class employee {
+    String name = new String("Nitish");
+    int age = 19;
+    String city = new String("Jammu");
+    public void Display() {
+        System.out.println("the name is "+name);
+        System.out.println("the age is "+age);
+        System.out.println("the city is "+city);
+    }
+}
+
